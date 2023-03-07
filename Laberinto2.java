1afdb2bd0445e2fa308b28455301c0b02c0b4d35
0610912238 <124665174+0610912238@users.noreply.github.com>
2023-03-07 11:54:17 +0100

    Update Laberinto2.java

--- Laberinto2.java
+++ Laberinto2.java
@@ -1,58 +1,57 @@
 import java.util.Random;
 
 public class Main {
-
     public static void main(String[] args) {
+        // Crear la matriz de 60 filas y 80 columnas
         char[][] matriz = new char[60][80];
-        Random rand = new Random();
-        
-        
-        //Inicializar la matriz con huecos en blanco
+
+        // Inicializar la matriz con espacios en blanco
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
                 matriz[i][j] = ' ';
             }
         }
-        
+
         // Generar 1200 asteriscos aleatorios
-        for (int i = 0; i < 1200; i++) {
-            int fila = rand.nextInt(60);
-            int columna = rand.nextInt(80);
-            matriz[fila][columna] = '*';
-        }
-        
-        // Generar ubicación aleatoria de la letra I
-       Random ini = new Random();
-        int inicio = 0;
-        while (inicio <1 ) {
-            int fila = ini.nextInt(60);
-            int columna = ini.nextInt(80);
+        Random random = new Random();
+        int asteriscosGenerados = 0;
+        while (asteriscosGenerados < 1200) {
+            int fila = random.nextInt(60);
+            int columna = random.nextInt(80);
             if (matriz[fila][columna] == ' ') {
-                matriz[fila][columna] = 'I';
-                inicio++;
+                matriz[fila][columna] = '*';
+                asteriscosGenerados++;
             }
         }
         
-        // Generar ubicación aleatoria de la letra F
-        Random fin = new Random();
-        int fina = 0;
-        while (fina <1 ) {
-            int fila = fin.nextInt(60);
-            int columna = fin.nextInt(80);
-            if (matriz[fila][columna] == ' ') {
-                matriz[fila][columna] = 'F';
-                fina++;
-            }
-        }
-        
+     // Generar ubicación aleatoria de la letra I
+        Random ini = new Random();
+         int inicio = 0;
+         while (inicio <1 ) {
+             int fila = ini.nextInt(60);
+             int columna = ini.nextInt(80);
+             if (matriz[fila][columna] == ' ') {
+                 matriz[fila][columna] = 'I';
+                 inicio++;
+             }
+         }
+         
+         // Generar ubicación aleatoria de la letra F
+         Random fin = new Random();
+         int fina = 0;
+         while (fina <1 ) {
+             int fila = fin.nextInt(60);
+             int columna = fin.nextInt(80);
+             if (matriz[fila][columna] == ' ') {
+                 matriz[fila][columna] = 'F';
+                 fina++;
+             }
+         }
+
         // Imprimir la matriz
-        for (int i = 0; i < 60; i++) {
-            for (int j = 0; j < 80; j++) {
-                if (matriz[i][j] == 0) {
-                    System.out.print(' '); // Imprimir un espacio en blanco si no hay ningún carácter en la ubicación
-                } else {
-                    System.out.print(matriz[i][j]);
-                }
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j]);
             }
             System.out.println();
         }
