4ad15f64a28567202923ff6022b81f883f49205a
0610912238 <124665174+0610912238@users.noreply.github.com>
2023-03-07 11:22:59 +0100

    Update Laberinto2.java

--- Laberinto2.java
+++ Laberinto2.java
@@ -1,33 +1,40 @@
 import java.util.Random;
 
 public class Main {
+
     public static void main(String[] args) {
-        // Crear la matriz de 60 filas y 80 columnas
         char[][] matriz = new char[60][80];
-
-        // Inicializar la matriz con espacios en blanco
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                matriz[i][j] = ' ';
-            }
-        }
-
+        Random rand = new Random();
+        
         // Generar 1200 asteriscos aleatorios
-        Random random = new Random();
-        int asteriscosGenerados = 0;
-        while (asteriscosGenerados < 1200) {
-            int fila = random.nextInt(60);
-            int columna = random.nextInt(80);
-            if (matriz[fila][columna] == ' ') {
-                matriz[fila][columna] = '*';
-                asteriscosGenerados++;
-            }
+        for (int i = 0; i < 1200; i++) {
+            int fila = rand.nextInt(60);
+            int columna = rand.nextInt(80);
+            matriz[fila][columna] = '*';
         }
-
+        
+        // Generar ubicación aleatoria de la letra I
+        int iFila = rand.nextInt(60);
+        int iColumna = rand.nextInt(80);
+        matriz[iFila][iColumna] = 'I';
+        
+        // Generar ubicación aleatoria de la letra F
+        int fFila = rand.nextInt(60);
+        int fColumna = rand.nextInt(80);
+        while (fFila == iFila && fColumna == iColumna) { // Asegurarse de que la F no esté en la misma ubicación que la I
+            fFila = rand.nextInt(60);
+            fColumna = rand.nextInt(80);
+        }
+        matriz[fFila][fColumna] = 'F';
+        
         // Imprimir la matriz
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                System.out.print(matriz[i][j]);
+        for (int i = 0; i < 60; i++) {
+            for (int j = 0; j < 80; j++) {
+                if (matriz[i][j] == 0) {
+                    System.out.print(' '); // Imprimir un espacio en blanco si no hay ningún carácter en la ubicación
+                } else {
+                    System.out.print(matriz[i][j]);
+                }
             }
             System.out.println();
         }
